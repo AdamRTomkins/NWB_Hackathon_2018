fc1179e9cff3c68d7799a2e50565b92f9a48abfb
Adam Tomkins <artomkins@googlemail.com>
2018-03-13 23:55:55 +0000

    add install

---
+++ install.sh
@@ -0,0 +1,6 @@
+pip install jupyterlab
+pip install elephant
+pip install h5py
+pip install seaborn
+pip install -U pynwb --find-links https://github.com/NeurodataWithoutBorders/pynwb/releases/tag/latest  --no-index
+pip install nwb
